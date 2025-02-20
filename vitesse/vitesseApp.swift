80c7a5f651039546bcbf8da4dad7ba060b017c7e
pascaljesenberger <pascal@jesenberger.fr>
2025-02-20 18:54:21 +0100

    Initial Commit

---
+++ vitesse/vitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  vitesseApp.swift
+//  vitesse
+//
+//  Created by pascal jesenberger on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct vitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vitesseTests/vitesseTests.swift
@@ -0,0 +1,17 @@
+//
+//  vitesseTests.swift
+//  vitesseTests
+//
+//  Created by pascal jesenberger on 20/02/2025.
+//
+
+import Testing
+@testable import vitesse
+
+struct vitesseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
